6d4be2ac5c6348ba0c1c04c7ff19900ce5d2289c
MatTeo-BT <aboutmet@gmail.com>
2023-12-22 13:02:34 +0100

    inserimento serie tv e specifiche

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template lang="">
     <div>
-        <AppSearch @serach="getMovies"/>
+        <AppSearchVue @search="getMovies"/>
         <AppMain :movies="moviesList"/>
     </div>
 </template>
 <script>
-import AppSearchVue from './components/Appsearch.vue';
+import AppSearchVue from './components/AppSearch.vue';
 import AppMain from './components/AppMain.vue';
-import axios from `axios`;
+import axios from "axios";
 
 export default {
     components: {
@@ -17,11 +17,12 @@ export default {
     data() {
         return {
             moviesList: [],
+            tvSeriesList: []
         }
     },
     methods: {
         getMovies(searchMovie = ``) {
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=dadc09536eef8ff2ae053c9ebd6144cb&query=', searchMovie)
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=dadc09536eef8ff2ae053c9ebd6144cb&query=' + searchMovie)
                 .then((response) => {
                     // handle success
                     console.log(response);
@@ -32,11 +33,26 @@ export default {
                     console.log(error);
                 });
 
+        },
+        getTvSeries(searchTvSeries = ``) {
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=' + searchTvSeries)
+                .then((response) => {
+                    // handle success
+                    console.log(response);
+                    this.tvSeriesList = response.data.results;
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                });
+
         }
     },
-    // created() {
-    //     this.getMovies();
-    // }
+
+    created() {
+        this.getMovies(),
+            this.getTvSeries();
+    }
 }
 </script>
 <style lang=""> 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,15 @@
     <div>
         <article v-for="movie in movies">
             {{movie.title}}
+            <p>Titolo originale: {{movie.original_title}}</p>
+            <p>Lingua originale: {{movie.original_language}}</p>
+            <p>Voto: {{movie.vote_average}}</p>
+        </article>
+        <article v-for='tvSeries in tvSeries'>
+            <p>Titolo: {{tvSeries.name}}</p>
+            <p>Titolo originale: {{tvSeries.original_name}}</p>
+            <p>Lingua originale: {{tvSeries.original_language}}</p>
+            <p>Voto: {{tvSeries.vote_average}}</p>
         </article>
     </div>
 </template>
@@ -10,6 +19,10 @@ export default {
     name: `AppMain`,
     props: {
         movies: {
+            type: Array,
+            required: true
+        },
+        tvSeries: {
             type: Array,
             required: true
         }
